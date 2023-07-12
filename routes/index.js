fe4be9374987581da9eca2cf8c60398ec5d9a853
席维海 <haizhilingyu@163.com>
2023-07-12 22:15:58 +0800

    first commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.get('/preview', async (ctx, next) => {
+  ctx.body = 'preview'
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+module.exports = router
