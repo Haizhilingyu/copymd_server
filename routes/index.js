c62429f393587d0ef43daf4d48295f188229f611
席维海 <haizhilingyu@163.com>
2023-07-18 20:38:42 +0800

    feat(数据缓存): 添加定时清除

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,23 @@ const router = require('koa-router')()
 const { marked } = require('marked');
 
 const cache = new Map(); // 声明一个Map对象用于缓存数据
+const expirationTime = 20 * 60 * 1000; // 过期时间
+
+// 清理过期数据的函数
+function clearExpiredData() {
+  const currentTime = Date.now();
+  
+  for (const [url, data] of cache.entries()) {
+    const { expiration } = data;
+    if (expiration <= currentTime) {
+      cache.delete(url);
+    }
+  }
+}
+
+// 每隔一定时间清理过期数据
+setInterval(clearExpiredData, 5 * 60 * 1000); // 每隔 5 分钟清理一次
+
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
   })
@@ -32,7 +49,7 @@ router.post('/url', async (ctx, next) => {
 })
 
 function saveData(markdown, url, password) {
-  const expiration = Date.now() + 20 * 60 * 1000; // 计算过期时间
+  const expiration = Date.now() + expirationTime; // 计算过期时间
   // 将markdown和password存入缓存
   cache.set(url, { markdown, password, expiration });
 }
