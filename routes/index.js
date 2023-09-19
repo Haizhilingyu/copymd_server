b96645acafc9747d689f61a369b00cfd82dfa7c8
席维海 <haizhilingyu@163.com>
2023-09-19 22:31:09 +0800

    添加私密访问，修复过期的页面也能查看的bug

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const router = require('koa-router')()
-const {marked} = require('marked');
-const {createClient} = require('@vercel/kv');
+const { marked } = require('marked');
+const { createClient } = require('@vercel/kv');
 
 const kv = createClient({
     url: process.env.KV_REST_API_URL,
@@ -9,18 +9,22 @@ const kv = createClient({
 router.get('/', async (ctx, next) => {
     // 获取当前请求的 URL 查询参数对象
     const queryParams = ctx.request.query;
-    let {markdown, url} = queryParams;
+    let { markdown, url, isPrivate } = queryParams;
     // 检查是否存在请求参数
     // 检查缓存中是否存在指定的 URL 的值
 
     if (Object.keys(queryParams).length > 0 && queryParams.url) {
         const cacheData = await getData(url)
-        markdown = cacheData.markdown;
-        url = queryParams.url;
+        if (cacheData.expiration && cacheData.expiration > Date.now()) {
+            markdown = cacheData.markdown;
+            url = queryParams.url;
+            isPrivate = cacheData.isPrivate;
+        }
     }
     await ctx.render('index', {
         markdown,
         url,
+        isPrivate
     })
 })
 
@@ -29,12 +33,12 @@ function isStringNotEmpty(str) {
 }
 
 router.post('/url', async (ctx, next) => {
-    const {markdown, url, password} = JSON.parse(ctx.request.body);
+    const { markdown, url, password, isPrivate } = JSON.parse(ctx.request.body);
     if (isStringNotEmpty(markdown) && isStringNotEmpty(url) && isStringNotEmpty(password)) {
         const cacheData = await getData(url)
-        if (cacheData && cacheData.markdown) {
+        if (cacheData && cacheData.expiration && cacheData.expiration > Date.now() && cacheData.markdown) {
             if (password === cacheData.password) {
-                await saveData(markdown, url, password);
+                await saveData(markdown, url, password, isPrivate);
                 console.log(url);
                 ctx.body = '{"code":200,"data":{"url":"' + url + '"},"message":"Save Success!"}';
             } else {
@@ -51,11 +55,11 @@ router.post('/url', async (ctx, next) => {
     }
 })
 
-async function saveData(markdown, url, password) {
+async function saveData(markdown, url, password, isPrivate) {
     try {
         const expirationTime = 20 * 60 * 1000;
         const expiration = Date.now() + expirationTime;
-        await kv.set(url, {markdown, password, expiration}, {ex: expirationTime});
+        await kv.set(url, { markdown, password, expiration, isPrivate }, { ex: expirationTime });
     } catch (error) {
         // Handle errors
     }
@@ -68,21 +72,32 @@ async function getData(url) {
 
 // 获取缓存数据，并检查是否过期
 router.get('/:url', async (ctx) => {
-  const url = ctx.params.url;
-  console.log(url);
-  const cacheData = await getData(url)
-  console.log(cacheData)
-  if (cacheData && cacheData.markdown) {
-      await ctx.render('preview', {content: marked(cacheData.markdown),expiration: cacheData.expiration, markdown: cacheData.markdown});
-  } else {
-      await ctx.render('error', {
-          message: "Page not found.",
-          error: {
-              status: 404,
-              stack: ""
-          }
-      });
-  }
+    const url = ctx.params.url;
+    console.log(url);
+    const cacheData = await getData(url)
+    console.log(cacheData)
+
+    if (cacheData && cacheData.expiration && cacheData.expiration > Date.now()) {
+        if (cacheData.isPrivate) {
+            const queryParams = ctx.request.query;
+            let { password } = queryParams;
+            if (password === cacheData.password) {
+                await ctx.render('preview', { "url": url, "content": marked(cacheData.markdown), "expiration": cacheData.expiration, "markdown": cacheData.markdown });
+            } else {
+                await ctx.render('verification', { "url": url })
+            }
+        } else if (cacheData && cacheData.markdown) {
+            await ctx.render('preview', { "content": marked(cacheData.markdown), "expiration": cacheData.expiration, "markdown": cacheData.markdown });
+        }
+    } else {
+        await ctx.render('error', {
+            message: "Page not found.",
+            error: {
+                status: 404,
+                stack: ""
+            }
+        });
+    }
 });
 
 module.exports = router
